e04ba8b391e334d7b0d5c5432cf0bdb3e9b4f5bf
Park Ju Hyung <qkrwngud825@gmail.com>
2023-06-01 16:28:21 -0300

    tulip: Optimize everything on preopt
    We have plenty of space left at /system

--- device.mk
+++ device.mk
@@ -401,14 +401,11 @@ PRODUCT_PACKAGES += \
     libpiex_shim  
 
 # Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := everything
 
 # System
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.binary_xml=false
-
-# Speed profile services and wifi-service to reduce RAM and storage.
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Always preopt extracted APKs to prevent extracting out of the APK for gms
 # modules.
